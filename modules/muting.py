d5169cdee33da9750184ea7600e88bb6dfac0ee7
Bluebify <bluebifydev@gmail.com>
2025-04-18 09:55:34 +0100

    Fixed bugs

--- modules/muting.py
+++ modules/muting.py
@@ -25,46 +25,41 @@ async def mute(update: Update, context: ContextTypes.DEFAULT_TYPE):
     if update.message.reply_to_message:
         target_user = update.message.reply_to_message.from_user
         username = target_user.username or target_user.first_name
-        username_type = 0 if target_user.username else 1
     
-    elif context.args:
-        username = context.args[0].lstrip('@')
-        try:
-            member = await context.bot.get_chat_member(chat_id, f"@{username}")
-            target_user = member.user
-        except Exception as e:
-            await update.message.reply_text(f"Couldn't find @{username} in this chat., {e}")
-            return
-        
+        """    elif context.args:
+                username = context.args[0].lstrip('@')
+                try:
+                    member = await context.bot.get_chat_member(chat_id, f"@{username}")
+                    target_user = member.user
+                except Exception as e:
+                    await update.message.reply_text(f"Couldn't find @{username} in this chat., {e}")
+                    return
+                
 
-        entities = list(update.message.parse_entities([MessageEntity.TEXT_MENTION]))
-        if len(entities) > 0:
-            ent = entities[0]
-        else:
-            ent = None
+                entities = list(update.message.parse_entities([MessageEntity.TEXT_MENTION]))
+                if len(entities) > 0:
+                    ent = entities[0]
+                else:
+                    ent = None
 
-        if ent:
-            target_user = ent.user
-        else:
-            if context.args[0][0] == "@":
-                await update.message.reply_text("I don't think I'm able to get this user, it'll be easier if you could reply to their message.")
+                if ent:
+                    target_user = ent.user
+                else:
+                    if context.args[0][0] == "@":
+                        await update.message.reply_text("I don't think I'm able to get this user, it'll be easier if you could reply to their message.")"""
     else:
-        await update.message.reply_text("Use /mute @{username} or reply to the user.")
+        await update.message.reply_text("Reply to the user's message with /mute")
         return
     if target_user:
         if await is_bot(update, context, target_user.id):
             await update.message.reply_text("I will not mute myself, thank you.")
         elif not await is_admin(update, context, target_user.id):
-            if not username_type:
-                await update.message.reply_text(f"Mute [@{username}](https://t.me/{username}) for how long?", reply_markup=reply_markup, parse_mode="MarkdownV2")
-            else:
-                await update.message.reply_text(f"Mute <a href='tg://user?id={target_user.id}'>{username}</a> for how long?", reply_markup=reply_markup, parse_mode="HTML")
+            await update.message.reply_text(f"Mute <a href='tg://user?id={target_user.id}'>{username}</a> for how long?", reply_markup=reply_markup, parse_mode="HTML")
         else:
             await update.message.reply_text("I can't mute an admin, unfortunately.")
     
     context.user_data["target_user"] = target_user
     context.user_data["username"] = username
-    context.user_data["username_type"] = username_type
     context.user_data["mute_dur"] = 0
 
 async def button(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -82,37 +77,30 @@ async def button(update: Update, context: ContextTypes.DEFAULT_TYPE):
     username = context.user_data.get('username')
 
     if not target_user:
-        await query.edit_message_text("No user selected for mute.")
+        await query.edit_message_text("Reply to the user's message with /mute")
         return
 
     if query.data == "add_an_hr":
         context.user_data["mute_dur"] += 1
-        if not context.user_data.get("username_type"):
-            await query.edit_message_text(f"Mute [@{username}](https://t.me/{username}) for {context.user_data.get('mute_dur')} hours?", reply_markup=reply_markup, parse_mode="MarkdownV2")
-        else:
-            await query.edit_message_text(f"Mute <a href='tg://user?id={target_user.id}'>{username}</a> for {context.user_data.get('mute_dur')} hours?", reply_markup=reply_markup, parse_mode="HTML")
+        await query.edit_message_text(f"Mute <a href='tg://user?id={target_user.id}'>{username}</a> for {context.user_data.get('mute_dur')} hours?", reply_markup=reply_markup, parse_mode="HTML")
 
     if query.data == "set_mute":
-        if not context.user_data.get("username_type"):
-            await query.edit_message_text(f"Muted [@{username}](https://t.me/{username}) for {context.user_data.get('mute_dur')} hours", parse_mode="MarkdownV2")
-        else:
+        if context.user_data.get("mute_dur") > 0:
             await query.edit_message_text(f"Muted <a href='tg://user?id={target_user.id}'>{username}</a> for {context.user_data.get('mute_dur')} hours", parse_mode="HTML")
-
-    if context.user_data.get("mute_dur") > 0:
-        until = datetime.utcnow() + timedelta(hours=context.user_data.get("mute_dur"))  # 1 hour mute
-        await context.bot.restrict_chat_member(
-            chat_id=update.effective_chat.id,
-            user_id=target_user.id,
-            permissions=ChatPermissions(can_send_messages=False),
-            until_date=until
-        )
-    else:
-        await query.edit_message_text(f"Muted {username}!")
-        await context.bot.restrict_chat_member(
-            chat_id=update.effective_chat.id,
-            user_id=target_user.id,
-            permissions=ChatPermissions(can_send_messages=False)
-        )
+            until = datetime.utcnow() + timedelta(hours=context.user_data.get("mute_dur"))  # 1 hour mute
+            await context.bot.restrict_chat_member(
+                chat_id=update.effective_chat.id,
+                user_id=target_user.id,
+                permissions=ChatPermissions(can_send_messages=False),
+                until_date=until
+            )
+        else:
+            await query.edit_message_text(f"Muted <a href='tg://user?id={target_user.id}'>{username}</a>!", parse_mode="HTML")
+            await context.bot.restrict_chat_member(
+                chat_id=update.effective_chat.id,
+                user_id=target_user.id,
+                permissions=ChatPermissions(can_send_messages=False)
+            )
 
 mute_handler = CommandHandler("mute", mute)
 mute_button_handler = CallbackQueryHandler(button)
